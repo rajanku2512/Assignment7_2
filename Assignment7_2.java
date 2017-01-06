79db3c2de8b4464528bd8a4bf859953e2d22728e
rajanku2512 <rajanku2512@gmail.com>
2017-01-07 03:19:10 +0530

    Add files via upload

---
+++ Assignment7_2.java
@@ -0,0 +1,13 @@
+
+public class Assignment7_2 {
+
+	public static void main(String[] args) {
+		/*       7_2Write a program to find a substring in a String by using an inbuilt method of String class.                   */
+		 StringBuffer XY = new StringBuffer("abcdefg"); 
+		  XY.insert(3, "   "); 
+		  System.out.println( "the two substrings can be===>"+XY );
+		    
+
+	}
+
+}
